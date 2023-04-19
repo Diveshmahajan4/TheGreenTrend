d099e2c2e51d7667793e66286de1460c6f0e5eea
DiveshMahajan <diveshmahajan04@gmail.com>
2023-04-20 03:01:39 +0530

    Hero Changes

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import Link from 'next/link'
+import { urlFor } from '@/lib/client'
 
-const Product = () => {
+const Product = ({product : {image , name, slug, price}}) => {
   return (
     <div>Product</div>
   )
